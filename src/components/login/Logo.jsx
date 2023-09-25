83f5056f581ad81e9c683964c14082bad55135ae
VadimTan <v.tanasiev1@gmail.com>
2023-09-25 16:39:08 +0300

    Login window

--- src/components/login/Logo.jsx
+++ src/components/login/Logo.jsx
@@ -1,11 +1,4 @@
-import DubX from '../../assets/logo-assets/DubXLogo.png';
-import Dub from '../../assets/logo-assets/Dub.png';
-import X from '../../assets/logo-assets/Xmark.png';
-import Intersect from '../../assets/logo-assets/Intersect.png';
-import Vector from '../../assets/logo-assets/Vector.png';
-import Vector1 from '../../assets/logo-assets/Vector1.png';
-import Vector2 from '../../assets/logo-assets/Vector2.png';
-import Vector3 from '../../assets/logo-assets/Vector3.png';
+import DubX from '../../assets/logo-assets/DubX.svg';
 import '../login/logo.css';
 
 export const Logo = () => {
@@ -14,34 +7,6 @@ export const Logo = () => {
 			<img
 				src={DubX}
 				className="logo-dubX"
-			/>
-			<img
-				src={Dub}
-				className="logo-dub"
-			/>
-			<img
-				src={X}
-				className="logo-x-mark"
-			/>
-			<img
-				src={Vector}
-				className="logo-vector"
-			/>
-			<img
-				src={Vector1}
-				className="logo-vector-1"
-			/>
-			<img
-				src={Intersect}
-				className="logo-intersect"
-			/>
-			<img
-				src={Vector2}
-				className="logo-vector-2"
-			/>
-			<img
-				src={Vector3}
-				className="logo-vector-3"
 			/>
 		</div>
 	);
